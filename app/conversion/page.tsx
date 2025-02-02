f51ea889e0acf60b47d8f1d0dee005842b9bd7a3
abdullah461 <abdullahabubakar461@.com>
2025-02-02 12:59:52 +0100

    Add Hero component to Conversion and Financing pages with titles and descriptions

--- app/conversion/page.tsx
+++ app/conversion/page.tsx
@@ -1,5 +1,6 @@
 import ContactSection from '@/components/Contact'
 import Footer from '@/components/Footer'
+import Hero from '@/components/hero'
 import Nav from '@/components/Nav'
 import React from 'react'
 
@@ -7,6 +8,10 @@ export default function Conversion() {
   return (
     <>
     <Nav />
+    <Hero
+    title='Autogas vehicle conversion'
+    description='We offer autogas vehicle conversion services for your fleet.'
+    />
     <ContactSection/>
     <Footer />
     </>
--- app/financing/page.tsx
+++ app/financing/page.tsx
@@ -1,5 +1,6 @@
 import ContactSection from '@/components/Contact'
 import Footer from '@/components/Footer'
+import Hero from '@/components/hero'
 import Nav from '@/components/Nav'
 import React from 'react'
 
@@ -7,6 +8,11 @@ export default function Financing() {
   return (
     <>
     <Nav />
+    <Hero
+    title='Autogas vehicle financing'
+    description='We offer flexible financing options for your autogas vehicle conversion.'
+    
+    />
     <ContactSection/>
     <Footer />
     </>
